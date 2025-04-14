4804d4db9d001967b758c5be78ebf2de1414af4a
Jake Powell <jjp68@cam.ac.uk>
2025-04-14 15:22:51 +0100

    add endpoint functions

--- R/pull_from_api.R
+++ R/pull_from_api.R
@@ -2,21 +2,182 @@
 #'
 #' @param url_path the API endpoint
 #' @param filter Addition objects to pass via include. (not currently in use)
-#' @param KEY API token
+#' @param token API token
+#' @param verbose Flag (TRUE/FALSE) for console printing.
 #'
 #' @return data frame of pulled information
 #' @export
-get_query <- function(url_path, filter='', KEY = ''){
-  # url_use = paste0(url_path, '?page=',page, filter)
-  url_use = url_path
-  req = httr2::request(url_use) |>
-    httr2::req_user_agent("OMEAPI") |>
-    httr2::req_auth_bearer_token(token = KEY) |>
-    httr2::req_perform()
+get_query <- function(url_path, filter='', token = '', verbose = F){
+  carry_on = T ; page = 1
+  data_all = list()
+  while(carry_on){
+    if(verbose) cli::cli_alert_info(paste0('Pulling page ', page))
+    url_use = paste0(url_path, '?pageNumber=',page, filter)
 
-  body = req$body |> rawToChar() |> jsonlite::prettify() |> jsonlite::fromJSON(simplifyDataFrame = T,flatten = T)
-  data = body
-  if(!is.data.frame(body)) data =  body$data |> as.data.frame()
+    req = httr2::request(url_use) |>
+      httr2::req_user_agent("OMEAPI") |>
+      httr2::req_auth_bearer_token(token = token) |>
+      httr2::req_perform()
 
+    body = req$body |> rawToChar() |> jsonlite::prettify() |> jsonlite::fromJSON(simplifyDataFrame = T,flatten = T)
+    if('list' %in% names(body)){
+      data = body$list |> as.data.frame()
+    }else{
+      data = body |> as.data.frame()
+    }
+    data_all[[page]] =  data
+
+    if(!'hasNextPage' %in% names(body)) break
+    if(!body$hasNextPage){
+      carry_on = F
+    }else{
+      page = page + 1
+    }
+  }
+  data = do.call(rbind, data_all)
   data
+  # url_use = url_path
+  # req = httr2::request(url_use) |>
+  #   httr2::req_user_agent("OMEAPI") |>
+  #   httr2::req_auth_bearer_token(token = token) |>
+  #   httr2::req_perform()
+  #
+  # body = req$body |> rawToChar() |> jsonlite::prettify() |> jsonlite::fromJSON(simplifyDataFrame = T,flatten = T)
+  # data = body
+  # if(!is.data.frame(body)) data =  body$data |> as.data.frame()
+  #
+  # data
+}
+
+
+
+#' Get base url
+#'
+#' @return
+cs_url <- function(){
+  return("https://mathscohortstudies-uat-backend.azurewebsites.net/api/Public/")
+}
+
+#' Get CS meta information
+#'
+#' @param token API token
+#'
+#' @return academic years database
+#' @export
+cs_academic_years <- function(token = NULL){
+ cs_endpoint(token, endpoint = 'academic-years')
+}
+
+
+#' @rdname cs_academic_years
+#' @export
+cs_school_years <- function(token = NULL){
+  cs_endpoint(token, endpoint = 'school-years')
+}
+
+#' @rdname cs_academic_years
+#' @export
+cs_response_types <- function(token = NULL){
+  cs_endpoint(token, endpoint = 'response-types')
+}
+
+#' @rdname cs_academic_years
+#' @export
+cs_response_value_groups <- function(token = NULL){
+  cs_endpoint(token, endpoint = 'response-value-groups')
+}
+
+#' @rdname cs_academic_years
+#' @export
+cs_person_types <- function(token = NULL){
+  cs_endpoint(token, endpoint = 'person-types')
+}
+
+#' @rdname cs_academic_years
+#' @export
+cs_cohorts <- function(token = NULL){
+  cs_endpoint(token, endpoint = 'cohorts')
+}
+
+#' @rdname cs_academic_years
+#' @export
+cs_school_types <- function(token = NULL){
+  cs_endpoint(token, endpoint = 'school-types')
+}
+
+#' Pull from meta endpoints
+#'
+#' @param token token
+#' @param endpoint endpoint
+#'
+cs_endpoint <- function(token = NULL, endpoint = ''){
+  if(!endpoint %in% c('academic-years', 'school-years', 'response-types', 'response-value-groups', 'person-types', 'school-types', 'cohorts', 'surveys')){
+    stop('Invalid endpoint')
+  }
+  url_use = paste0(cs_url(), endpoint)
+  get_query(url_use, token = check_token(token))
+}
+
+
+#' Pull from CS API - schools/school endpoint
+#'
+#' @param token token
+#' @param include Related objects to include. Valid values are: 'class', 'student', 'teacher'. Comma separated list of objects to include.
+#' @param id school ID in CS database. If NA, all schools are pulled.
+#'
+#' @return data.frame of pulled information
+#' @export
+#'
+#' @examples
+cs_schools <- function(token = NULL, id = NA, include =NA){
+
+  if(any(!include %in% c('class', 'teacher', 'student', NA))) stop('Invalid `include` values!')
+
+  url_use = paste0(cs_url(), 'schools')
+  if(!is.na(id)){
+    url_use = paste0(url_use, '/', id)
+
+  }
+  if(!is.na(include[1])){
+    filter = paste0('&include=',paste0(include, collapse = '%2C%20'))
+  }else{
+    filter =''
+  }
+
+  out = get_query(url_use, token = check_token(token),filter = filter)
+
+}
+
+
+#' Pull from CS API - surveys / surveys\{id\} / survey\{id\}/response endpoints
+#'
+#' @param token token
+#' @param id survey ID in CS database. If NA, all schools are pulled.
+#'
+#' @return data.frame of pulled information
+#' @export
+#'
+#' @examples
+cs_surveys <- function(token = NULL, id = NA){
+
+  url_use = paste0(cs_url(), 'surveys')
+  if(!is.na(id)){
+    url_use = paste0(url_use, '/', id)
+
+  }
+
+  get_query(url_use, token = check_token(token))
+
+}
+
+#' @rdname cs_surveys
+#' @export
+cs_surveys_responses <- function(token = NULL, id = NA){
+  if(is.na(id)) stop('Require id!')
+  assert_is(id, 'numeric')
+
+  url_use = paste0(cs_url(), 'surveys/',id,'/responses')
+
+  get_query(url_use, token = check_token(token))
+
 }
---
+++ R/token_management.R
@@ -0,0 +1,19 @@
+check_token <- function(x = NULL ){
+  tmp = x
+  if (is.null(x)) tmp = Sys.getenv("OME_CS_TOKEN")
+  assert_is(tmp, 'character')
+  tmp
+}
+
+#' Set CS token to the environment.
+#'
+#' @param token token
+#'
+#' @export
+#'
+#' @details
+#' Given an CS token to access the CS database, store the token in the local environment. This means you don't need to pass the token to each call to the API. Moreover, you can set once and delete thus not have the token on display in R files.
+#'
+set_token <- function(token){
+  Sys.setenv("OME_CS_TOKEN" = token)
+}
